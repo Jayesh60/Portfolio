14e6b2ec52c59494a77f3933429c69ce5cfd4f53
Jayesh Wadhe <jwadhe46@gmail.com>
2023-06-06 21:13:59 +0530

    modified hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,17 +28,17 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-20 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute lg:hidden  bottom-20 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className="w-[25px] h-[40px] rounded-3xl border-2 border-secondary flex justify-center items-start p-2">
            <motion.div
-            animate={{ y: [0, 24, 0]}}
+            animate={{ y: [0, 12, 0]}}
             transition={{
               duration:1.5,
               repeat: Infinity,
               repeatType:"loop"
             }}
-            className="w-3 h-3 rounded-full bg-secondary mb-1 "
+            className="w-1.5 h-1.5 rounded-full bg-secondary  "
            />
           </div>
         </a>
